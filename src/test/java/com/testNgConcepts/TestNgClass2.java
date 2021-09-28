85518ea6268888f3b15f26479ed9a0974ec644c7
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-09-28 08:09:51 +0530

    28 Sep 2021 selenium first program to launch chrome driver

---
+++ src/test/java/com/testNgConcepts/TestNgClass2.java
@@ -0,0 +1,87 @@
+package com.testNgConcepts;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class TestNgClass2 {
+	WebDriver driver;
+  @Test(priority=-1)
+  public void validateGMOonlineLoadedSuccessfully() {
+	  System.out.println("inside validateGMOonlineLoadedSuccessfully");
+	  String Actualtitle=driver.getTitle();
+	  String expectedtitle ="Welcome to Green Mountain Outpost";
+	  System.out.println(Actualtitle);
+	  Assert.assertEquals(Actualtitle, expectedtitle);
+  }
+  
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("inside beforeMethod");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("inside afterMethod");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("inside beforeClass");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("inside afterClass");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("inside beforeTest");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("inside afterTest");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("inside beforeSuite");
+	  WebDriverManager.chromedriver().setup();
+	  driver= new ChromeDriver();
+	  driver.get("http://demo.borland.com/gmopost/");
+	  driver.manage().window().maximize();
+	  //implicit wait
+	  //explicit wait 
+	  //fluent wait
+	  //implicit wait
+	  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	  //driver.findElement(By.name("bSubmit")).click();
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("inside afterSuite");
+  }
+
+}
