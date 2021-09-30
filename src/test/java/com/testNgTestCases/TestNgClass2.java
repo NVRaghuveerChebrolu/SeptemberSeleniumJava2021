b73529c70ce04d1e23d73b62ca986e328c095831
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-09-30 08:13:08 +0530

    30sep2021 total price calculation test case

--- src/test/java/com/testNgTestCases/TestNgClass2.java
+++ src/test/java/com/testNgTestCases/TestNgClass2.java
@@ -15,6 +15,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
@@ -24,11 +26,12 @@ import org.testng.annotations.AfterSuite;
 
 public class TestNgClass2 {
 	WebDriver driver;
+	
   @Test(priority=-1)
   public void validateGMOonlineLoadedSuccessfully() {
 	  System.out.println("inside validateGMOonlineLoadedSuccessfully");
 	  String Actualtitle=driver.getTitle();
-	  String expectedtitle ="Welcome to Green Mountain Outpost";
+	  String expectedtitle ="Welcome to Green Mountain Outpos";
 	  System.out.println(Actualtitle);
 	  Assert.assertEquals(Actualtitle, expectedtitle);
   }
@@ -39,11 +42,27 @@ public class TestNgClass2 {
 	  driver.findElement(By.name("bSubmit")).click();
 	  driver.findElement(By.xpath("//input[@type='text' and @name='QTY_BACKPACKS']")).sendKeys("4");
 	  //table/tbody/tr[3]/td[3]
-	
 	  String qtyPriceFrameBackpack = driver.findElement(By.xpath("//*[contains(text(),'$ 179.95')]")).getText();
 	  System.out.println(qtyPriceFrameBackpack);
+	  driver.findElement(By.xpath("//input[@value='Place An Order']")).click();
   }
   
+  @Test
+  public void ValidatePriceCalculationInPlaceorderPage(){
+	  System.out.println("inside ValidatePriceCalculationInPlaceorderPage");
+	  String ActualTitle=driver.findElement(By.xpath("//h1[contains(text(),'Place Order')]")).getText();
+	  String ExpectedTile = "Place Order";
+	  Assert.assertEquals(ActualTitle, ExpectedTile);
+	  String UnitPriceBackPack = driver.findElement(By.xpath("//table/tbody/tr[2]/td[4]")).getText();
+	  String PriceQtyBackPack = UnitPriceBackPack.substring(2).trim();
+	  System.out.println(PriceQtyBackPack);
+	  Float ExpectedTotalPriceBackPack = Float.parseFloat(PriceQtyBackPack)*4;
+	  System.out.println("ExpectedTotalPriceBackPack:"+ExpectedTotalPriceBackPack);
+	  String AcutalTotalPriceBackPack  = driver.findElement(By.xpath("//table/tbody/tr[2]/td[5]")).getText().substring(2).trim();
+	  Float ExpectedPrice=Float.parseFloat(AcutalTotalPriceBackPack);
+	  System.out.println("ExpectedPrice:"+ExpectedPrice);
+	  Assert.assertEquals(ExpectedPrice, ExpectedTotalPriceBackPack);
+  }
   
   @BeforeMethod
   public void beforeMethod() {
@@ -84,7 +103,9 @@ public class TestNgClass2 {
 	  driver.manage().window().maximize();
 	  //implicit wait
 	  //explicit wait 
-	  //fluent wait
+	  /*WebDriverWait wait = new WebDriverWait(driver,90);
+	  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("")));
+	  *///fluent wait
 	  //implicit wait
 	  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 	  //driver.findElement(By.name("bSubmit")).click();
